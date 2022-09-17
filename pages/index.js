4b05d0d06a8c8f53e2c7e69faab9a91a6149bceb
Gabriel Lucas <25166715+Night3lf@users.noreply.github.com>
2022-09-16 19:08:49 -0600

    Removed Lorum ipsum

--- pages/index.js
+++ pages/index.js
@@ -15,27 +15,28 @@ export default function Home() {
 
       <div className="bg-amber-100 w-full min-h-screen md:py-8">
         <div className="md:mx-4">
-        <div className="bg-white max-w-4xl mx-auto p-16 md:flex space-y-2">
-            <div class="w-full">
-                <div className=''>
-                    <Image src={HeadShotImage} width={150} height={150} />
-                </div>
-                </div>
-                <div className='px-6 space-y-4 border-l border-gray-200'>
-                    <div class="">
-                        <h1 className="text-3xl font-medium">Ric Lucas</h1>
-                        <h2 className="text-gray-600">Senior Construction Project Manager</h2>
-                    </div>
+            <div className="bg-white max-w-4xl mx-auto p-16 md:flex space-y-2">
+                <div className="w-full">
+                    <div className='px-6 space-y-4 md:flex md:space-x-4'>
+                        <div>
+                            <Image src={HeadShotImage} width={150} height={150} />
+                        </div>
+                        <div className="border-l border-gray-200 pl-2">
+                            <h1 className="text-3xl font-medium">Ric Lucas</h1>
+                            <h2 className="text-gray-600">Senior Construction Project Manager</h2>
+                        </div>
 
-                    <div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ipsum augue, porta non odio nec, molestie pharetra dui. Vivamus sit amet sagittis purus.s id porttitor. Pellentesque nulla leo, imperdiet fermentum viverra sit amet, blandit eget risus. Mauris ac eros at tell convallis efficitur.</p>
+                        {/* <div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ipsum augue, porta non odio nec, molestie pharetra dui. Vivamus sit amet sagittis purus.s id porttitor. Pellentesque nulla leo, imperdiet fermentum viverra sit amet, blandit eget risus. Mauris ac eros at tell convallis efficitur.</p>
+                        </div> */}
+
                     </div>
+                </div>
 
                 </div>
+
+                <embed className="bg-white w-full max-w-4xl mx-auto p-16 space-x-4 flex h-screen md:mt-8" src={'/WEBSITE_PROJECT_PROFILES.pdf'} type="application/pdf"/>
             </div>
-
-            <embed className="bg-white w-full max-w-4xl mx-auto p-16 space-x-4 flex h-screen md:mt-8" src={'/WEBSITE_PROJECT_PROFILES.pdf'} type="application/pdf"/>
-        </div>
 
       </div>
 
